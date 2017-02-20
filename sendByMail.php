8aac93d74d4700367c217f65c178e125bf69cc24
Marinerito-PC\Marinerito <afgfrias@gmail.com>
2017-02-20 22:22:54 +0100

    New phpMailer version working fine from contacto.php

--- sendByMail.php
+++ sendByMail.php
@@ -1,32 +1,42 @@
 <?php
 
+require 'PHPMailerAutoload.php';
 
-$email_to = "btrescordoba@gmail.com";
-$email_subject = "Contacto desde el sitio web";
-$contact_Email = $_POST['contact_Email'];
+try {
 
+	$email_to = "btrescordoba@gmail.com";
+	$email_subject = "Contacto desde el sitio web";
+	$contact_Email = $_POST['contact_Email'];
 
-if(!isset($_POST['contact_Name']) ||
-!isset($_POST['contact_Email']) ||
-!isset($_POST['contact_Comment'])) {
+	$mail = new PHPMailer(true); //Nueva instancia, con las excepciones habilitadas
 
-	echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
-	echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
-	die();
-}else {
+	$body = "Detalles del formulario de contacto:\n\n";
+	$body .= "Nombre: " . $_POST['contact_Name'] . "\n";
+	$body .= "E-mail: " . $_POST['contact_Email'] . "\n";
+	$body .= "Comentarios: " . $_POST['contact_Comment'] . "\n\n";
 
-	$email_message = "Detalles del formulario de contacto:\n\n";
-	$email_message .= "Nombre: " . $_POST['contact_Name'] . "\n";
-	$email_message .= "E-mail: " . $_POST['contact_Email'] . "\n";
-	$email_message .= "Comentarios: " . $_POST['contact_Comment'] . "\n\n";
-
-
-	// Ahora se envía el e-mail usando la función mail() de PHP
-	$headers = 'From: noreply@ualasince.com'."\r\n".
-	'X-Mailer: PHP/' . phpversion();
-
-	@mail($email_to, $email_subject, $email_message, $headers);
+	$mail->IsSMTP();                           // Usamos el metodo SMTP de la clase PHPMailer
+	$mail->SMTPAuth   = true;                  // habilitado SMTP autentificación
+	$mail->Port       = 587;                    // puerto del server SMTP
+	$mail->Host       = "smtp.gmail.com"; // SMTP server
+	
+	$mail->Username   = "btrescordoba@gmail.com";     // SMTP server Usuario
+	$mail->Password   = "3peluquero2017";            // SMTP server password
+	$mail->From       = "noreply@ualasince1990.com"; //Remitente de Correo
+	$mail->FromName   = "Uala"; //Nombre del remitente
+	$mail->AddAddress($email_to);
+	$mail->Subject  = "Contacto desde sitio web"; //Asunto del correo
+	$mail->MsgHTML($body);
+	$mail->IsHTML(true); // Enviar como HTML
+	$mail->Send();//Enviar
+	
 
 	header('Location: /gatete/index.php');
+	
+	
+}catch (phpmailerException $e) {
+	echo $e->errorMessage();//Mensaje de error si se produciera.
 }
+
+
 ?>
